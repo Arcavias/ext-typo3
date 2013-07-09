9f5f1f7673f0cb91fbce27402b3c3c1feb155a21
Norbert <n.sendetzky@metaways.de>
2013-07-09 18:24:02 +0200

    Implements customer list and type managers for TYPO3 based on fe_users_list and fe_users_list_type tables

--- lib/custom/setup/default/schema/mysql/customer.sql
+++ lib/custom/setup/default/schema/mysql/customer.sql
@@ -80,20 +80,121 @@ CONSTRAINT "fk_t3feuad_langid"
 
 CREATE INDEX "idx_t3feuad_refid" ON "fe_users_address" ("refid");
 
-CREATE INDEX "idx_t3feuad_sid_ln_fn" ON "mshop_customer_address" ("siteid", "lastname", "firstname");
+CREATE INDEX "idx_t3feuad_sid_ln_fn" ON "fe_users_address" ("siteid", "lastname", "firstname");
 
-CREATE INDEX "idx_t3feuad_sid_ad1_ad2" ON "mshop_customer_address" ("siteid", "address1", "address2");
+CREATE INDEX "idx_t3feuad_sid_ad1_ad2" ON "fe_users_address" ("siteid", "address1", "address2");
 
-CREATE INDEX "idx_t3feuad_sid_post_ci" ON "mshop_customer_address" ("siteid", "postal", "city");
+CREATE INDEX "idx_t3feuad_sid_post_ci" ON "fe_users_address" ("siteid", "postal", "city");
 
-CREATE INDEX "idx_t3feuad_sid_rid" ON "mshop_customer_address" ("siteid", "refid");
+CREATE INDEX "idx_t3feuad_sid_rid" ON "fe_users_address" ("siteid", "refid");
 
-CREATE INDEX "idx_t3feuad_sid_lastname" ON "mshop_customer_address" ("siteid", "lastname");
+CREATE INDEX "idx_t3feuad_sid_lastname" ON "fe_users_address" ("siteid", "lastname");
 
-CREATE INDEX "idx_t3feuad_sid_postal" ON "mshop_customer_address" ("siteid", "postal");
+CREATE INDEX "idx_t3feuad_sid_postal" ON "fe_users_address" ("siteid", "postal");
 
-CREATE INDEX "idx_t3feuad_sid_city" ON "mshop_customer_address" ("siteid", "city");
+CREATE INDEX "idx_t3feuad_sid_city" ON "fe_users_address" ("siteid", "city");
 
-CREATE INDEX "idx_t3feuad_sid_addr1" ON "mshop_customer_address" ("siteid", "address1");
+CREATE INDEX "idx_t3feuad_sid_addr1" ON "fe_users_address" ("siteid", "address1");
 
-CREATE INDEX "idx_t3feuad_sid_rid" ON "mshop_customer_address" ("siteid", "email");
+CREATE INDEX "idx_t3feuad_sid_rid" ON "fe_users_address" ("siteid", "email");
+
+
+--
+-- Table structure for table `fe_users_list_type`
+--
+
+CREATE TABLE "fe_users_list_type" (
+	-- Unique id
+	"id" INTEGER NOT NULL AUTO_INCREMENT,
+	-- site id, references mshop_locale_site.id
+	"siteid" INTEGER NOT NULL,
+	-- domain
+	"domain" VARCHAR(32) NOT NULL,
+	-- code
+	"code"  VARCHAR(32) NOT NULL COLLATE utf8_bin,
+	-- Name of the list type
+	"label" VARCHAR(255) NOT NULL,
+	-- Status (0=disabled, 1=enabled, >1 for special)
+	"status" SMALLINT NOT NULL,
+	-- Date of last modification of this database entry
+	"mtime" DATETIME NOT NULL,
+	-- Date of creation of this database entry
+	"ctime" DATETIME NOT NULL,
+	-- Editor who modified this entry at last
+	"editor" VARCHAR(255) NOT NULL,
+CONSTRAINT "pk_t3feulity_id"
+	PRIMARY KEY ("id"),
+CONSTRAINT "unq_t3feulity_sid_dom_code"
+	UNIQUE ("siteid", "domain", "code"),
+CONSTRAINT "fk_t3feulity_siteid"
+	FOREIGN KEY ("siteid")
+	REFERENCES "mshop_locale_site" ("id")
+	ON UPDATE CASCADE
+	ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE INDEX "idx_t3feulity_sid_status" ON "fe_users_list_type" ("siteid", "status");
+
+CREATE INDEX "idx_t3feulity_sid_label" ON "fe_users_list_type" ("siteid", "label");
+
+CREATE INDEX "idx_t3feulity_sid_code" ON "fe_users_list_type" ("siteid", "code");
+
+
+--
+-- Table structure for table `fe_users_list`
+--
+
+CREATE TABLE "fe_users_list" (
+	-- Unique list id
+	"id" INTEGER NOT NULL AUTO_INCREMENT,
+	-- text id (parent id)
+	"parentid" INTEGER NOT NULL,
+	-- site id, references mshop_locale_site.id
+	"siteid" INTEGER NOT NULL,
+	-- typeid
+	"typeid" INTEGER NOT NULL,
+	-- domain (e.g.: text, media)
+	"domain" VARCHAR(32) NOT NULL,
+	-- Reference of the object in given domain
+	"refid" VARCHAR(32) NOT NULL,
+	-- Valid from
+	"start" DATETIME DEFAULT NULL,
+	-- Valid until
+	"end" DATETIME DEFAULT NULL,
+	-- Precedence rating
+	"pos" INTEGER NOT NULL,
+	-- Date of last modification of this database entry
+	"mtime" DATETIME NOT NULL,
+	-- Date of creation of this database entry
+	"ctime" DATETIME NOT NULL,
+	-- Editor who modified this entry at last
+	"editor" VARCHAR(255) NOT NULL,
+CONSTRAINT "pk_t3feuli_id"
+	PRIMARY KEY ("id"),
+CONSTRAINT "unq_t3feuli_sid_pid_dm_rid_tid"
+	UNIQUE ("siteid", "parentid", "domain", "refid", "typeid"),
+CONSTRAINT "fk_t3feuli_siteid"
+	FOREIGN KEY ("siteid")
+	REFERENCES "mshop_locale_site" ("id")
+	ON UPDATE CASCADE
+	ON DELETE CASCADE,
+CONSTRAINT "fk_t3feuli_typeid"
+	FOREIGN KEY ( "typeid" )
+	REFERENCES "fe_users_list_type" ("id")
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE INDEX "idx_t3feuli_parentid" ON "fe_users_list" ("parentid");
+
+CREATE INDEX "idx_t3feuli_sid_start_end" ON "fe_users_list" ("siteid", "start", "end");
+
+CREATE INDEX "idx_t3feuli_sid_rid_dom_tid" ON "fe_users_list" ("siteid", "refid", "domain", "typeid");
+
+CREATE INDEX "idx_t3feuli_pid_sid_rid" ON "fe_users_list" ("parentid", "siteid", "refid");
+
+CREATE INDEX "idx_t3feuli_pid_sid_start" ON "fe_users_list" ("parentid", "siteid", "start");
+
+CREATE INDEX "idx_t3feuli_pid_sid_end" ON "fe_users_list" ("parentid", "siteid", "end");
+
+CREATE INDEX "idx_t3feuli_pid_sid_pos" ON "fe_users_list" ("parentid", "siteid", "pos");
