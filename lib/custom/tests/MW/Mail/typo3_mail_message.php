ae74bb8630be21c35112de1ed4bc5a4e3910bc18
Norbert <n.sendetzky@metaways.de>
2013-09-12 19:06:50 +0200

    Fixes coding style

--- lib/custom/tests/MW/Mail/typo3_mail_message.php
+++ lib/custom/tests/MW/Mail/typo3_mail_message.php
@@ -3,17 +3,49 @@
 /**
  * TYPO3 mail stub required for tests.
  */
-class t3lib_mail_Message
+class T3lib_Mail_Message
 {
-	public function setCharset( $charset ) {}
-	public function addFrom( $email, $name ) {}
-	public function addTo( $email, $name = null ) {}
-	public function addCc( $email, $name = null ) {}
-	public function addBcc( $email, $name = null ) {}
-	public function addReplyTo( $email, $name = null ) {}
-	public function setSender( $email, $name = null ) {}
-	public function setSubject( $subject ) {}
-	public function setBody( $message ) {}
-	public function addPart( $message, $mimetype ) {}
-	public function send() {}
+	public function setCharset( $charset )
+	{
+	}
+
+	public function addFrom( $email, $name )
+	{
+	}
+
+	public function addTo( $email, $name = null )
+	{
+	}
+
+	public function addCc( $email, $name = null )
+	{
+	}
+
+	public function addBcc( $email, $name = null )
+	{
+	}
+
+	public function addReplyTo( $email, $name = null )
+	{
+	}
+
+	public function setSender( $email, $name = null )
+	{
+	}
+
+	public function setSubject( $subject )
+	{
+	}
+
+	public function setBody( $message )
+	{
+	}
+
+	public function addPart( $message, $mimetype )
+	{
+	}
+
+	public function send()
+	{
+	}
 }
